cc17d5b95a08eb9e10310eb2076f0ea071471263
Striilir <Striilir@gmail.com>
2020-06-12 10:46:46 +0200

    Partie 2 : ajout du TextField

--- src/main/java/fr/univ_amu/iut/ihm/TraceurDeFonction.java
+++ src/main/java/fr/univ_amu/iut/ihm/TraceurDeFonction.java
@@ -1,13 +1,26 @@
 package fr.univ_amu.iut.ihm;
 
-import fr.univ_amu.iut.utilitaires.Analyseur;
-import fr.univ_amu.iut.utilitaires.ErreurDeSyntaxe;
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import javafx.scene.control.TextField;
 
-public class TraceurDeFonction {
+import java.lang.module.Configuration;
+import java.util.concurrent.ScheduledExecutorService;
+
+public class TraceurDeFonction extends Application {
 
   void calculCoeffTransformationsAffines() {
   }
-  	
+
+  GridPane root = new GridPane();
+
   int transformationAffineY(double y) {
     return 0;
   }
@@ -17,24 +30,38 @@ public class TraceurDeFonction {
   }
 
   private void setIds() {
-/*      votreIdentificateur.setId("texteAAnalyser");
-      votreIdentificateur.setId("resultatAnalyse");
-      votreIdentificateur.setId("demandeAnalyser");
-      votreIdentificateur.setId("demandeTracer");
-      votreIdentificateur.setId("demandeEffacer");
-      votreIdentificateur.setId("zoneTraceCourbe");
-      votreIdentificateur.setId("choixXMin");
-      votreIdentificateur.setId("choixXMax");
-      votreIdentificateur.setId("choixEspacementX_v1");
-      votreIdentificateur.setId("choixEspacementX_v2");
-      votreIdentificateur.setId("choixEspacementY_v1");
-      votreIdentificateur.setId("choixEspacementY_v2");
-      votreIdentificateur.setId("choixCouleur");
-      votreIdentificateur.setId("choixEpaisseur");
-      votreIdentificateur.setId("segmentsATracer");
-      votreIdentificateur.setId("quadrillage");
-      votreIdentificateur.setId("axeX");
-      votreIdentificateur.setId("axeY");*/
+/*      pane.setId("resultatAnalyse");
+      pane.setId("demandeAnalyser");
+      pane.setId("demandeTracer");
+      pane.setId("demandeEffacer");
+      pane.setId("zoneTraceCourbe");
+      pane.setId("choixXMin");
+      pane.setId("choixXMax");
+      pane.setId("choixEspacementX_v1");
+      pane.setId("choixEspacementX_v2");
+      pane.setId("choixEspacementY_v1");
+      pane.setId("choixEspacementY_v2");
+      pane.setId("choixCouleur");
+      pane.setId("choixEpaisseur");
+      pane.setId("segmentsATracer");
+      pane.setId("quadrillage");
+      pane.setId("axeX");
+      pane.setId("axeY");*/
+  }
+
+  public void start(Stage stage) {
+      stage.setTitle("Traceur de fonction");
+      Pane root = new Pane();
+      Scene scene = new Scene(root);
+      stage.setWidth(600);
+      stage.setHeight(650);
+      stage.setResizable(false);
+      TextField textField = new TextField("exp(-x * 0.2) * sin(x)");
+      textField.setMinWidth(120);
+      root.setPadding(new Insets(10));
+      root.getChildren().add(textField);
+      stage.setScene(scene);
+      stage.show();
   }
 
 }
